5850e137ba9154e1356f7a0703c655a33a20a383
Bouter <davidMJbos@gmail.com>
2014-05-15 15:36:24 +0200

    Rev3

--- LED_Server.js
+++ LED_Server.js
@@ -43,8 +43,7 @@ var board = new firmata.Board("../../../../../dev/ttyATH0",function(err) {
                     if (err) console.log(err);
                     console.log(res);
                 });
-                // this gets called each time there is a new sensor reading
-                
+                // this gets called each time there is a new sensor reading!!
                 board.analogRead(board.A0, function(val){
                 console.log(val);
                 });
