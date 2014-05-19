e3805c62cc8ba3714832b4c70fcb1f085f8d8776
Bouter <davidMJbos@gmail.com>
2014-05-19 10:45:10 +0200

    op

--- LED_Server.js
+++ LED_Server.js
@@ -4,7 +4,6 @@ var layout = {fileopt : "extend", filename : "Humidity2!"};
 var ledPin = 7;
 var analogPin = 3;
 var data;
-var time = new Date();
 var firmata = require('firmata');
 var board = new firmata.Board("../../../../../dev/ttyATH0",function(err) {
     if (err) {
@@ -17,20 +16,17 @@ var board = new firmata.Board("../../../../../dev/ttyATH0",function(err) {
         board.pins[board.analogPins[3]];
 
         board.pinMode(ledPin, board.MODES.OUTPUT);
-        board.pinMode(analogPin,board.MODES.ANALOG)
+        board.pinMode(analogPin,board.MODES.ANALOG);
         //Read analog pin 3
-        board.analogRead(analogPin, function(val)
-        {
-                
-            data = {x : time , y : val};
-
-    
+        board.analogRead(analogPin, function (val) {  
+            data = {x : new Date() , y : val};
         });
         var strings = require('querystring');
         var http = require('http');
-        http.createServer(function(request, response){
-            console.log(strings.parse(request.url).value);
-            if ((strings.parse(request.url).value) == 'HIGH'){
+        http.createServer(function (request, response) {
+            var urlObject = strings.parse(request.url)
+            console.log(urlObject.value);
+            if ((urlObject.value) == 'HIGH') {
                 board.digitalWrite(ledPin, board.HIGH);
             } else {
                 board.digitalWrite(ledPin, board.LOW);
@@ -41,11 +37,7 @@ var board = new firmata.Board("../../../../../dev/ttyATH0",function(err) {
         }).listen(8080);
         console.log('Listening on port 8080 ...');
         console.log('Board Ready plotting');
-        
-   // });
-//});
-
-}
+    }
 });
 
 // initialize the plotly graph
