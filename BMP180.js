b7bdf4fbf0653461b4a2afbf53f931c0cf5c6ec9
Bouter <davidMJbos@gmail.com>
2014-05-19 14:23:29 +0200

    ty

---
+++ BMP180.js
@@ -0,0 +1,22 @@
+var app = {
+	board,
+	init: function (board) {
+		app.board = board;
+	},
+	requestTemperature: function () {
+		var datapress;
+		app.board.sendI2CWriteRequest(0x77,[0x2E]);
+		app.board.sendI2CReadRequest(0x77,2,function(temp){
+			var press = app.getCalculatedTemperature(temp);
+            datapress = {x : new Date(), y : press};
+            console.log(press);
+        });
+        return datapress;
+	}
+}
+
+app.prototype.getCalculatedTemperature = function (temp) {
+	return temp;
+};
+
+exports.module = app;
