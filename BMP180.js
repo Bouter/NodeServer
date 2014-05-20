9a9f0b612e525be9d1d277361a44d4feac6468fb
Bouter <davidMJbos@gmail.com>
2014-05-20 14:18:13 +0200

    sdf

--- BMP180.js
+++ BMP180.js
@@ -123,7 +123,7 @@ Bmp180.prototype = {
 													that.calibrated = true;
 													console.log(that.calibrated);
 													that.requestTemperature();
-													that.getCalculatedTemperature(that.currentTemp, that.coeffs);
+													getCalculatedTemperature(that.currentTemp, that.coeffs);
 												});
 											});
 										});
