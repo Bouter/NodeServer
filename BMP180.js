0ab772fc945afb8c63236e139f5e49e2294a87f9
Bouter <davidMJbos@gmail.com>
2014-05-21 16:23:28 +0200

    18 to  16

--- BMP180.js
+++ BMP180.js
@@ -65,8 +65,8 @@ var getCalculatedPressure = function (UP, coeffs) {
 		p = (B7 / B4) * 2;
 	}
 	X1 = (p >> 8) * (p >> 8);
-	X1 = (X1 * 3038) >> 18;
-	X2 = (-7357 * p)  >> 18;
+	X1 = (X1 * 3038) >> 16;
+	X2 = (-7357 * p)  >> 16;
 	p = p + ((X1 + X2 + 3791) >> 4);
 	console.log(UP ,X1, X2, B5, B4, B7);
 	console.log("Pressure ",p);
