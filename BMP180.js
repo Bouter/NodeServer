95e9d5a51f85227d8d22183509d55e2e15601c83
Bouter <davidMJbos@gmail.com>
2014-05-21 17:29:52 +0200

    sc

--- BMP180.js
+++ BMP180.js
@@ -75,7 +75,7 @@ var getCalculatedPressure = function (UP, coeffs) {
 var getCalculatedAltitude = function (p) {
 	var altitude;
 	var pw;
-	pw = Math.pow((p/100)/101325),(1903/1000));
+	pw = Math.pow(((p/100)/101325),(1903/1000));
 	console.log("pw ", p);
 	altitude = 44330.0 * (1.0 - (Math.pow(((p/100.0) /101325.0),(1903/1000))));
 	console.log("Altitude ", altitude);
