91bee106986ad585f3ea8cc282dd032085b73256
Cinezaster <toon@appsaloon.be>
2014-05-28 15:02:37 +0200

    rename file

--- LED_Server.js
+++ LED_Server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 var initdata = [{x:[], y:[], stream:{token:'9np05kx444', maxpoints:200}},
                 {x:[], y:[], stream:{token:'3joif1t1q4', maxpoints:200}},
                 {x:[], y:[], stream:{token:'97go390uxk', maxpoints:200}}];
-var layout = {fileopt : "overwrite", filename : "Humidity2!"};
+var layout = {fileopt : "extend", filename : "Humidity2a!"};
 var ledPin = 7;
 var analogPin = 3;
 var data;
@@ -43,14 +43,14 @@ var board = new firmata.Board("/dev/ttyATH0",function(err) {
             } 
         });
 
-        io.sockets.on('connection', function (socket) {
+       /* io.sockets.on('connection', function (socket) {
             setInterval(function () {
                 socket.broadcast.emit("temperatuur", 'Temp : ' + pressureBoard.getCurrentTemp() + '°C');
                 socket.broadcast.emit("luchtdruk", 'Druk : ' + pressureBoard.getCurrentPress() + 'kPa');
                 socket.broadcast.emit("hoogte", 'Hoogte : ' + pressureBoard.getCurrentAltitude() + 'cm');
 
             }, 10000);
-        });
+        });*/
        
         console.log('Board Ready plotting');
     }
@@ -58,16 +58,16 @@ var board = new firmata.Board("/dev/ttyATH0",function(err) {
 
 
 // initialize the plotly graph
-plotly.plot(initdata, layout, function (err, msg) {
+board.on('ready', function() {
+    plotly.plot(initdata, layout, function (err, msg) {
     if (err) return console.log(err);
     console.log(msg);
     //once it's initialized, create a plotly stream to pipe your data!
-    console.log('Check1');
         
     var streamTemp = plotly.stream('9np05kx444', function (err, res) 
     {
         console.log(err, res);
-        console.log("stream clossed");
+        console.log("stream closed");
         clearInterval(loop);  
     });
     var streamPress = plotly.stream('3joif1t1q4', function (err, res) {
@@ -87,13 +87,15 @@ plotly.plot(initdata, layout, function (err, msg) {
             console.log(streamObjectPress);
             var streamObjectAltitude = JSON.stringify({ x : new Date(), y : pressureBoard.getCurrentAltitude()});
             console.log(streamObjectAltitude);
-        }
+        };
         streamTemp.write(streamObjectTemp+'\n');
         console.log("streaming");
         streamPress.write(streamObjectPress+'\n');
         streamAltitude.write(streamObjectAltitude+'\n');
     },60000);  
 });            
+});
+
 
             
 
